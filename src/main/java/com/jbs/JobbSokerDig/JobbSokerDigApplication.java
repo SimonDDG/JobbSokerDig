573489a33569f2d58bdd85b9e649df6e7e8308cf
SimonDDG <simon.douchka@gmail.com>
2019-12-23 13:47:26 +0100

    first

---
+++ src/main/java/com/jbs/JobbSokerDig/JobbSokerDigApplication.java
@@ -0,0 +1,13 @@
+package com.jbs.JobbSokerDig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbSokerDigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbSokerDigApplication.class, args);
+	}
+
+}
