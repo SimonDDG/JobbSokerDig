a4cbb5bb121bc49a0e96c94cca09bbac938c865f
robinsardella <robinsardella@gmail.com>
2020-01-09 13:29:11 +0100

    Header, footer, and general design for index-page, about-page

--- src/main/java/com/jbs/JobbSokerDig/security/SecurityConfig.java
+++ src/main/java/com/jbs/JobbSokerDig/security/SecurityConfig.java
@@ -31,9 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/images/adroit-logo-transparent.png").permitAll()
                 .antMatchers("/images/logo-transparent-white.png").permitAll()
                 .antMatchers("/images/adroit-transparent-logo-small.png").permitAll()
-                .antMatchers("/profileStyle.css").permitAll()
-                .antMatchers("/headerStyle.css").permitAll()
-
+                .antMatchers("/style.css").permitAll()
                 //Public for testing purpose
                 .antMatchers("/test").permitAll()
                 .antMatchers("/test2").permitAll()
