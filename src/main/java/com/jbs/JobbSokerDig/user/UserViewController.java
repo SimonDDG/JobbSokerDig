ad91b8d9e3c36f0c6cabe6f1663d06981456f069
Linus Lundqvist <l.lundqvist@gmail.com>
2020-01-10 14:31:57 +0100

    Added user MustHaves and preferredBenefits to userEditPage + moved userEditProfile logic from userViewController to userEditProfileViewLogic to clean up class.

--- src/main/java/com/jbs/JobbSokerDig/user/UserViewController.java
+++ src/main/java/com/jbs/JobbSokerDig/user/UserViewController.java
@@ -4,6 +4,7 @@ import com.jbs.JobbSokerDig.company.SoftOffer;
 import com.jbs.JobbSokerDig.service.*;
 import com.jbs.JobbSokerDig.values.Benefit;
 import com.jbs.JobbSokerDig.values.Qualification;
+import com.jbs.JobbSokerDig.viewLogic.UserEditProfileViewLogic;
 import com.jbs.JobbSokerDig.viewLogic.ViewLogic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -38,6 +39,9 @@ public class UserViewController {
     @Autowired
     ViewLogic viewLogic;
 
+    @Autowired
+    UserEditProfileViewLogic userEditProfileViewLogic;
+
     @GetMapping("/userMain")
     public String getUserMain(){
 
@@ -47,86 +51,59 @@ public class UserViewController {
     @GetMapping("/userProfile")
     public String getUserProfile(HttpServletRequest request, Model model){
 
-        UserCandidate userCandidate = getCurrentUserCandidate(request);
+        UserCandidate userCandidate = userEditProfileViewLogic.getCurrentUserCandidate(request);
         model.addAttribute("userCandidate", userCandidate);
 
-        List<UserQualification> userQualification = getCurrentUserCandidateQualifications(userCandidate);
+        List<UserQualification> userQualification = userEditProfileViewLogic.getCurrentUserCandidateQualifications(userCandidate);
         model.addAttribute("userQualification", userQualification);
 
         List<UserPreference> userPreference = userPreferenceService.getUserPreference(userCandidate.getUserCandidateId());
         model.addAttribute("userPreference", userPreference);
-
-        String test = userPreference.get(0).benefit.getBenefit();
-
-        System.out.println(test);
 
         return "userProfile";
     }
 
     @GetMapping("/userEditProfile")
     public String getEditUserProfile(HttpServletRequest request, Model model) {
-        UserCandidate userCandidate = getCurrentUserCandidate(request);
+        UserCandidate userCandidate = userEditProfileViewLogic.getCurrentUserCandidate(request);
 
-        List<UserQualification> userQualification = getCurrentUserCandidateQualifications(userCandidate);
+        List<UserQualification> userQualification = userEditProfileViewLogic.getCurrentUserCandidateQualifications(userCandidate);
         model.addAttribute("userQualification", userQualification);
 
         List<Qualification> qualifications = qualificationService.getAllQualifications();
-        List<Qualification> checkedQualificationsList = checkQualificationsAgainstUserCandidateQualifications(userCandidate, qualifications);
+        List<Qualification> checkedQualificationsList = userEditProfileViewLogic.checkQualificationsAgainstUserCandidateQualifications(userCandidate, qualifications);
 
         List<List<Qualification>> splittedQualifications = viewLogic.splitQualificationList(checkedQualificationsList, 5);
         model.addAttribute("splittedQualifications", splittedQualifications);
 
+        List<UserPreference> userPreference = userPreferenceService.getUserPreference(userCandidate.getUserCandidateId());
+        model.addAttribute("userPreference", userPreference);
+
         List<Benefit> benefits = benefitService.getAllBenefits();
-        List<List<Benefit>> splittedBenefits = viewLogic.splitBenefitList(benefits, 5);
+        List<Benefit> checkedBenefitsList = userEditProfileViewLogic.checkBenefitsAgainstUserCandidateBenefits(userCandidate, benefits);
+        List<List<Benefit>> splittedBenefits = viewLogic.splitBenefitList(checkedBenefitsList, 5);
         model.addAttribute("splittedBenefits", splittedBenefits);
 
+        List<UserPreference> userMustHaves = userEditProfileViewLogic.checkUserMustHavePreferances(userCandidate, userPreference);
+        model.addAttribute("userMustHaves", userMustHaves);
 
+        List<UserPreference> notUserMustHaves = userEditProfileViewLogic.isNotUserMustHavePreferances(userCandidate, userPreference);
+        model.addAttribute("notUserMustHaves", notUserMustHaves);
 
         return "userEditProfile";
     }
 
-    private List<Qualification> checkQualificationsAgainstUserCandidateQualifications(UserCandidate userCandidate, List<Qualification> qualifications) {
 
-        List<UserQualification> userQualification = getCurrentUserCandidateQualifications(userCandidate);
-        System.out.println("SKRIVER VI UT USER QAL LISTAN?!" + userQualification);
-
-        List<Qualification> rL = qualifications;
-
-
-
-        for (UserQualification uq : userQualification) {
-            for (int j = 0; j < qualifications.size(); j++) {
-                System.out.println("PRINTAR VARJE UQ " + uq.getUserQualification().getQualification());
-                System.out.println("PRINTAR VARJE SQ " + qualifications.get(j).getQualification());
-                if (uq.getUserQualification().getQualification().equals(qualifications.get(j).getQualification())) {
-                    System.out.println("PRINTAR VARJE UQ2 " + uq.getUserQualification().getQualification());
-                    System.out.println("PRINTAR VARJE SQ2 " + qualifications.get(j).getQualification());
-                    rL.remove(j);
-
-                }
-            }
-        }
-        System.out.println("DETTA AER RETURNLISTEN!!" + rL);
-        return rL;
-    }
 
 
     @GetMapping("/userMyOffers")
     public String getUserMyOffers(HttpServletRequest request, Model model) {
 
-        List<SoftOffer> softOffers = softOfferService.getSoftOfferForUser(getCurrentUserCandidate(request).getUserCandidateId());
+        List<SoftOffer> softOffers = softOfferService.getSoftOfferForUser(userEditProfileViewLogic.getCurrentUserCandidate(request).getUserCandidateId());
         model.addAttribute("softOffers", softOffers);
 
         return "userMyOffers";
     }
 
-    private UserCandidate getCurrentUserCandidate(HttpServletRequest request) {
-        UserCandidate userCandidate = userCandidateService.getUserCandidate(request);
-        return userCandidate;
-    }
 
-    private List<UserQualification> getCurrentUserCandidateQualifications(UserCandidate userCandidate) {
-        List<UserQualification> userQualification = userQualificationService.getUserQualification(userCandidate.getUserCandidateId());
-        return userQualification;
-    }
 }
