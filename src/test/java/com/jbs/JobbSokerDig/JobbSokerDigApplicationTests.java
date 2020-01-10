5457a68bf179bf19004d4852f158ea4d6a946f56
robinsardella <robinsardella@gmail.com>
2020-01-10 11:55:36 +0100

    MatchService

--- src/test/java/com/jbs/JobbSokerDig/JobbSokerDigApplicationTests.java
+++ src/test/java/com/jbs/JobbSokerDig/JobbSokerDigApplicationTests.java
@@ -1,15 +1,26 @@
 package com.jbs.JobbSokerDig;
 
+import com.jbs.JobbSokerDig.service.MatchService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 class JobbSokerDigApplicationTests {
+
+	@Autowired
+	MatchService matchService;
 
 	@Test
 	void contextLoads() {
 	}
 
-
+	@Test
+	public void testarMatchningsArray() {
+		ArrayList<Integer> testArray = matchService.howManyQualificationsMatched();
+		System.out.println(testArray);
+	}
 
 }
