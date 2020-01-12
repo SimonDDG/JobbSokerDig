fdb4385e4c8a4b98d46772f530729abe0cd7a6a3
SimonDDG <simon.douchka@gmail.com>
2020-01-12 19:29:34 +0100

    just a save, wok in progress, got all relevant lists from repo.

--- src/main/java/com/jbs/JobbSokerDig/service/UserPreferenceService.java
+++ src/main/java/com/jbs/JobbSokerDig/service/UserPreferenceService.java
@@ -2,11 +2,16 @@ package com.jbs.JobbSokerDig.service;
 
 import com.jbs.JobbSokerDig.repositorys.UserPreferenceRepository;
 import com.jbs.JobbSokerDig.repositorys.UserQualificationRepository;
+import com.jbs.JobbSokerDig.user.UserCandidate;
 import com.jbs.JobbSokerDig.user.UserPreference;
 import com.jbs.JobbSokerDig.user.UserQualification;
+import com.jbs.JobbSokerDig.values.Benefit;
+import com.jbs.JobbSokerDig.viewLogic.UserEditProfileViewLogic;
+import com.jbs.JobbSokerDig.viewLogic.ViewLogic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Service
@@ -15,10 +20,75 @@ public class UserPreferenceService {
     @Autowired
     UserPreferenceRepository userPreferenceRepository;
 
+    @Autowired
+    UserPreferenceService userPreferenceService;
+
+    @Autowired
+    BenefitService benefitService;
+
+    @Autowired
+    UserEditProfileViewLogic userEditProfileViewLogic;
+
+    @Autowired
+    ViewLogic viewLogic;
+
+
+
     public List<UserPreference> getUserPreference(Long userCandidateId){
 
         List<UserPreference> userPreference = userPreferenceRepository.getUserPreferenceRepo(userCandidateId);
 
         return userPreference;
     }
+
+    public void saveUserMustHaves(String[] mustHaves, HttpServletRequest request) {
+
+        for (int i = 0; i < mustHaves.length; i++) {
+            System.out.println("must: " + mustHaves[i]);
+        }
+        System.out.println("----");
+
+        UserCandidate userCandidate = userEditProfileViewLogic.getCurrentUserCandidate(request);
+
+        List<UserPreference> userPreference = userPreferenceService.getUserPreference(userCandidate.getUserCandidateId());
+        List<Benefit> benefits = benefitService.getAllBenefits();
+        List<Benefit> checkedBenefitsList = userEditProfileViewLogic.checkBenefitsAgainstUserCandidateBenefits(userCandidate, benefits);
+
+        
+        List<List<Benefit>> splittedBenefits = viewLogic.splitBenefitList(checkedBenefitsList, 5);
+//        model.addAttribute("splittedBenefits", splittedBenefits);
+
+        List<UserPreference> userMustHaves = userEditProfileViewLogic.checkUserMustHavePreferances(userCandidate, userPreference);
+//        model.addAttribute("userMustHaves", userMustHaves);
+
+        List<UserPreference> notUserMustHaves = userEditProfileViewLogic.isNotUserMustHavePreferances(userCandidate, userPreference);
+//        model.addAttribute("notUserMustHaves", notUserMustHaves);
+
+    }
+
+    public void saveUserOptionalPreferences(String[] optionalPreferences, HttpServletRequest request) {
+
+        for (int i = 0; i < optionalPreferences.length; i++) {
+            System.out.println("opt: " + optionalPreferences[i]);
+        }
+        System.out.println("----");
+
+    }
 }
+
+/*
+
+Bra att ha kod, ta bort sen.
+
+        List<UserPreference> userPreference = userPreferenceService.getUserPreference(userCandidate.getUserCandidateId());
+        List<Benefit> benefits = benefitService.getAllBenefits();
+        List<Benefit> checkedBenefitsList = userEditProfileViewLogic.checkBenefitsAgainstUserCandidateBenefits(userCandidate, benefits);
+        List<List<Benefit>> splittedBenefits = viewLogic.splitBenefitList(checkedBenefitsList, 5);
+        model.addAttribute("splittedBenefits", splittedBenefits);
+
+        List<UserPreference> userMustHaves = userEditProfileViewLogic.checkUserMustHavePreferances(userCandidate, userPreference);
+        model.addAttribute("userMustHaves", userMustHaves);
+
+        List<UserPreference> notUserMustHaves = userEditProfileViewLogic.isNotUserMustHavePreferances(userCandidate, userPreference);
+        model.addAttribute("notUserMustHaves", notUserMustHaves);
+     */
--- src/main/java/com/jbs/JobbSokerDig/user/UserFunctionController.java
+++ src/main/java/com/jbs/JobbSokerDig/user/UserFunctionController.java
@@ -1,5 +1,6 @@
 package com.jbs.JobbSokerDig.user;
 
+import com.jbs.JobbSokerDig.service.UserPreferenceService;
 import com.jbs.JobbSokerDig.service.UserQualificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,9 @@ public class UserFunctionController {
     @Autowired
     UserQualificationService userQualificationService;
 
+    @Autowired
+    UserPreferenceService userPreferenceService;
+
     @PostMapping("/saveUserSettings")
     public String saveUserProfile(HttpServletRequest request, @RequestParam(value = "checkboxName", required = false) String[] qualificationIds) {
 
@@ -24,10 +28,7 @@ public class UserFunctionController {
     @PostMapping("/saveUserMustHave")
     public String saveUserMustHave(HttpServletRequest request, @RequestParam(value = "checkboxName", required = false) String[] mustHaves) {
 
-        for (int i = 0; i < mustHaves.length; i++) {
-            System.out.println("must: " + mustHaves[i]);
-        }
-        System.out.println("----");
+        userPreferenceService.saveUserMustHaves(mustHaves, request);
 
         return "redirect:/userEditProfile";
     }
@@ -35,12 +36,9 @@ public class UserFunctionController {
     @PostMapping("/saveUserOptionalPreference")
     public String saveUserOptionalPreference(HttpServletRequest request, @RequestParam(value = "checkboxName", required = false) String[] optionalPreferences) {
 
-        for (int i = 0; i < optionalPreferences.length; i++) {
-            System.out.println("opt: " + optionalPreferences[i]);
-        }
-        System.out.println("----");
+        userPreferenceService.saveUserOptionalPreferences(optionalPreferences, request);
 
         return "redirect:/userEditProfile";
     }
-    
+
 }
